ae53b1c43ffadcdd6a72f09cddc285a927779f6d
warisButt <waris.shayansolutions@gmail.com>
2023-02-20 20:23:38 +0500

    Complete

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 document.getElementById("mySearch").size = "50";
-document.getElementById("region").size = "50";
+document.getElementById("region").size = "30";
 let cardAPI = document.querySelector(".allapi");
 
 const darkModefun = () => {
   let element = document.body;
+  let navColor = document.getElementById("nav");
+  if (navColor.style.background == "grey") {
+    navColor.style.background = "rgb(235, 232, 232)";
+  } else {
+    navColor.style.background = "grey";
+  }
+
+  let searchColor = document.getElementById("mySearch");
+  if (searchColor.style.background == "grey") {
+    searchColor.style.background = "rgb(245, 241, 241)";
+    // searchColor.style.color = "black";
+  } else {
+    searchColor.style.background = "grey";
+    // searchColor.style.color = "white";
+  }
   element.classList.toggle("dark-mode");
 };
 
-function showData() {
-  const url = "https://restcountries.com/v3.1/all";
-  apiData(url);
-}
-
-const apiData = async (url) => {
-  try {
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
-    data.map((element) => {
-      let cardCreate = document.createElement("div");
-      cardCreate.classList.add("cardCreate");
-      cardCreate.innerHTML = `
+const createCard = (element) => {
+  let cardCreate = document.createElement("div");
+  cardCreate.classList.add("cardCreate");
+  cardCreate.innerHTML = `
       <div class="countryCard"  >
       <div class="flag">
       <img src="${element.flags.png}" alt="flag" width="280" height="180" >
@@ -33,93 +38,63 @@ const apiData = async (url) => {
       </div>
       </div>
       `;
+  cardCreate.addEventListener("click", () => {
+    window.location.assign("./newPage.html");
+    sessionStorage.setItem("value", element.name.common);
+  });
+  cardAPI.appendChild(cardCreate);
+};
 
-      cardCreate.addEventListener("click", () => {
-        window.location.assign("./newPage.html");
-        sessionStorage.setItem("value", element.name.common);
-      });
-      cardAPI.appendChild(cardCreate);
-    });
+const apiData = async (url) => {
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    data.forEach((element) => createCard(element));
   } catch (error) {
     console.log(error);
   }
 };
 
-function searchByName() {
-  let form = document.getElementById("form");
-  let input = document.getElementById("mySearch");
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    let inputValue = input.value;
-    console.log(inputValue);
-    cardAPI.innerText = " ";
-    let response = await fetch(
-      `https://restcountries.com/v3.1/name/${inputValue}`
-    );
-    let data = await response.json();
-    data.map((element) => {
-      let newCard = document.createElement("div");
-      newCard.classList.add("newCard");
-      newCard.innerHTML = `
-    <div class="countryCard"  >
-    <div class="flag">
-    <img src="${element.flags.png}" alt="flag" width="280" height="180" >
-    </div>
-    <div class="description">
-    <h3><span>${element.name.common}</span></h3>
-    <p id="pop"><span><b>Population:</b> </span>${element.population.toLocaleString()}</p>
-    <p><span><b>Region:</b> </span>${element.region}</p>
-    <p><span><b>Capital:</b> </span>${element.capital}</p>
-    </div>
-    </div>
-    `;
-      cardAPI.appendChild(newCard);
-      cardAPI.addEventListener("click", () => {
-        window.location.assign("./newPage.html");
-        sessionStorage.setItem("value", element.name.common);
-      });
-    });
-  });
-}
+const searchByName = async (inputValue) => {
+  console.log(inputValue);
+  cardAPI.innerText = " ";
+  let response = await fetch(
+    `https://restcountries.com/v3.1/name/${inputValue}`
+  );
+  let data = await response.json();
+  data.forEach((element) => createCard(element));
+};
 
-function searchByRegion() {
-  let secondForm = document.getElementById("secondForm");
-  let region = document.getElementById("region");
-  secondForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    let regValue = region.value;
-    console.log(regValue[1].name);
-    cardAPI.innerText = " ";
-    let response = await fetch(
-      `https://restcountries.com/v3.1/region/${regValue}?fullText=true`
-    );
-    let regData = await response.json();
+const searchByRegion = async (regValue) => {
+  console.log(regValue[1].name);
+  cardAPI.innerText = " ";
+  let response = await fetch(
+    `https://restcountries.com/v3.1/region/${regValue}?fullText=true`
+  );
+  let regData = await response.json();
+  regData.forEach((element) => createCard(element));
+};
 
-    regData.map((element) => {
-      let regCard = document.createElement("div");
-      regCard.classList.add("regCard");
-      regCard.innerHTML = `
-    <div class="countryCard"  >
-    <div class="flag">
-    <img src="${element.flags.png}" alt="flag" width="280" height="180" >
-    </div>
-    <div class="description">
-    <div class="classNam">
-    <h3><span>${element.name.common}</span></h3></div>
-    <p id="pop"><span><b>Population:</b> </span>${element.population.toLocaleString()}</p>
-    <p><span><b>Region:</b> </span>${element.region}</p>
-    <p><span><b>Capital:</b> </span>${element.capital}</p>
-    </div>
-    </div>
-    `;
-      cardAPI.appendChild(regCard);
-      regCard.addEventListener("click", (e) => {
-        console.log(`${e.target.value}Clicked, after Search by Filter`);
-        window.location.assign("./newPage.html");
-        sessionStorage.setItem("value", element.name.common);
-      });
-    });
-  });
-}
+const showData = () => {
+  const url = "https://restcountries.com/v3.1/all";
+  apiData(url);
+};
+
+const form = document.getElementById("form");
+const input = document.getElementById("mySearch");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  let inputValue = input.value;
+  searchByName(inputValue);
+});
+
+const secondForm = document.getElementById("secondForm");
+const region = document.getElementById("region");
+secondForm.addEventListener("change", async (e) => {
+  e.preventDefault();
+  let regValue = region.value;
+  searchByRegion(regValue);
+});
+
 showData();
-searchByRegion();
--- newPage.js
+++ newPage.js
@@ -2,56 +2,128 @@ const darkModefun2 = () => {
   console.log("Button clicked");
   let element = document.body;
   element.classList.toggle("dark-mode");
+  let navColor = document.getElementById("nav");
+  if (navColor.style.background == "grey") {
+    navColor.style.background = "rgb(255 255 255)";
+  } else {
+    navColor.style.background = "grey";
+  }
+
+  let searchColor = document.getElementById("mySearch");
+  if (searchColor.style.background == "grey") {
+    searchColor.style.background = "rgb(245, 241, 241)";
+    // searchColor.style.color = "black";
+  } else {
+    searchColor.style.background = "grey";
+    // searchColor.style.color = "white";
+  }
+  let backButtonColor = document.getElementById("backButton");
+  if (backButtonColor.style.background == "grey") {
+    backButtonColor.style.background = "white";
+    // searchColor.style.color = "black";
+  } else {
+    backButtonColor.style.background = "grey";
+    // searchColor.style.color = "white";
+  }
 };
+
+async function fetchData(countryCode) {
+  try {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/alpha/${countryCode}`
+    );
+    const data = await response.json();
+    const countryName = data[0].name.common;
+
+    return countryName;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 let getValue = sessionStorage.getItem("value");
 let cardAPI = document.querySelector(".allapi");
-// let getRegion = sessionStorage.getItem("avalue");
+
 const apiData = async () => {
   try {
     let response = await fetch(
       `https://restcountries.com/v3.1/name/${getValue}?fullText=true`
-      // `https://restcountries.com/v3.1/region/${getRegion}`
     );
     let data = await response.json();
     console.log(data);
-    data.map((element) => {
-      let cardCreate = document.createElement("div");
-      cardCreate.classList.add("cardCreate");
-      cardCreate.innerHTML = `
-      <div class="countryCard"  >
-      <div class="flag">
-      <img id="flagImage" src="${
-        element.flags.png
-      }" alt="flag" width="580" height="410" >
-      </div>
-      <div class="description">
-      <h2><span>${element.name.common}</span></h2>
-      <br>
-    
-      <p id="pop"><span><b>Native Name:</b> </span>${
-        Object.values(element.name.nativeName)[0].common
-      }</p>
-      <p id="pop"><span><b>Population:</b> </span>${element.population.toLocaleString()}</p>
-      <p><span><b>Region:</b> </span>${element.region}</p>
-      <p><span><b>Sub Region:</b> </span>${element.subregion}</p>
-      <p><span><b>Capital:</b> </span>${element.capital}</p>
-      
-      <p><span><b>Top Level Domain:</b> </span>${element.tld}</p>
-      <p><b>Currencies:</b>  ${Object.values(element.currencies)[0].name}</p>
-      <p><b>Languages:</b> ${Object.values(element.languages)}</p>
-     
-      <p ><span><b>Border Countries:</b> </span><button class="btn btn-tertiary" id="borders">${
-        element.borders
+
+    try {
+      data.map(async (element) => {
+        let cardCreate = document.createElement("div");
+        cardCreate.classList.add("cardCreate");
+
+        let borderCountries = [];
+
+        if (Array.isArray(element.borders)) {
+          borderCountries = await Promise.all(
+            element.borders.map((borderCode) => fetchData(borderCode))
+          );
+        }
+
+        cardCreate.innerHTML = `
+            <div id="card2">
+              <div class="flag">
+                <img id="flagImage" src="${
+                  element.flags.png
+                }" alt="flag" width="580" height="410">
+              </div>
+             
+             
+               
+              
+              
+
+              <div class="des1">
+                
+              <h2><span>${element.name.common}</span></h2>
+              <br>
+                <p id="pop"><span><b>Native Name:</b> </span>${
+                  Object.values(element.name.nativeName)[0].common
+                }</p>
+                <p id="pop"><span><b>Population:</b> </span>${element.population.toLocaleString()}</p>
+                <p><span><b>Region:</b> </span>${element.region}</p>
+                <p><span><b>Sub Region:</b> </span>${element.subregion}</p>
+                <p><span><b>Capital:</b> </span>${element.capital}</p>
+                </div>
+
+                <div class ="des2">
+                
+                <p><span><b>Top Level Domain:</b> </span>${element.tld}</p>
+                <p><b>Currencies:</b>  ${
+                  Object.values(element.currencies)[0].name
+                }</p>
+                <p><b>Languages:</b> ${Object.values(element.languages)}</p>
+                </div>
+
+                <div class="des3">
+                <p><span><b>Border Countries:</b> </span>
+    <div class="button-container">
+      ${
+        borderCountries.length > 0
+          ? borderCountries
+              .map(
+                (borderCountry) => `<button id="btn">${borderCountry}</button>`
+              )
+              .join(" ")
+          : "None"
       }
-      </button></p>
-      
-      </div>
-      </div>
-      
-      `;
-      cardAPI.appendChild(cardCreate);
-    });
+    </div>
+   
+  </p>
+
+              </div>   
+            </div>
+          `;
+        cardAPI.appendChild(cardCreate);
+      });
+    } catch (error) {
+      console.error("Error occurred during data mapping: ", error);
+    }
   } catch (error) {
     console.log(error);
   }
